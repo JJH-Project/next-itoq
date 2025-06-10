91d9d797b91a4fee50b87fb1e62cdc2dbd7a765d
Jeong <qra2222@gmail.com>
2025-06-11 08:58:05 +0900

    netx.config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['res.cloudinary.com'],
+      domains: ['res.cloudinary.com'],
     },
-    experimental: {
-        turbo: {
-            resolveAlias: {
-                '@vercel/turbopack-next/internal/font/google/font': 'next/font/google',
-            },
-        },
+    turbopack: {
+      resolveAlias: {
+        '@vercel/turbopack-next/internal/font/google/font': 'next/font/google',
+      },
     },
-}
-
-module.exports = nextConfig 
+  }
+  
+  module.exports = nextConfig
